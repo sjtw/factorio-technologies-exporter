a509db65c9ddffc0507ba29d5e9eab1f49e5420d
Simon <simonjtweed@gmail.com>
2024-10-25 23:00:15 +0100

    Update data.lua

--- data.lua
+++ data.lua
@@ -5,12 +5,7 @@ data:extend({
     order = "d[quickbar]-b[export]",
     action = "lua",
     localised_name = {"export-researched-technogies_shortcut.title"},
-    icon = {
-      filename = "__core__/graphics/export.png",
-      priority = "extra-high-no-scale",
-      size = 64,
-      scale = 1,
-      flags = {"icon"}
-    }
+    icon = "__core__/graphics/export.png",
+    small_icon = "__core__/graphics/export.png"
   }
 })
